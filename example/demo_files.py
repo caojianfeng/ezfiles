e14504131337730ba51508c3e7d191a4f8a76262
曹建峰 <caojianfeng@innobuddy.com>
2019-12-02 19:45:46 +0800

    init

---
+++ example/demo_files.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os
+from ezfiles import readlines, writelines, readstr, readjson
+
+
+def brother_path(filename): return os.path.join(
+    os.path.dirname(__file__), filename)
+
+
+def read_as_lines():
+    lines1 = readlines(brother_path('cfg.txt'))
+    print(f"lines1:{lines1}")
+    '''
+    lines1:['hello', 'ezflines']
+    '''
+    lines2 = readlines(brother_path('cfg.txt'), False)
+    print(f"lines2:{lines2}")
+    '''
+    lines2:['hello\n', 'ezflines\n']
+    '''
+
+
+def write_as_lines():
+    lines = ['hello', 'ezflines']
+    writelines(lines, brother_path('cfg.txt'))
+    '''
+    cfg.txt:
+    hello
+    ezflines
+
+    '''
+    writelines(lines, brother_path('cfg_oneline.txt'), False)
+    '''
+    cfg_oneline.txt:
+    helloezflines
+    '''
+
+
+def read_as_string():
+    string = readstr(brother_path('cfg.txt'))
+    print(f"read_as_string:\n{string}")
+
+
+def read_as_json():
+    json_obj = readjson(brother_path('cfg.json'))
+    print(f"read_as_json: type = {type(json_obj)}")
+    images = json_obj["images"]
+    for image in images:
+        print(f"read_as_json: image = {image}")
+
+
+if __name__ == "__main__":
+    write_as_lines()
+    read_as_lines()
+    read_as_string()
+    read_as_json()
